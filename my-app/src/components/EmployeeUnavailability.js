6502edb107aa1056ef61f758b1ae8e5b011e8d49
Hemangee De <hemangee4700@gmail.com>
2026-01-12 21:47:22 +0530

    Unavailability Page

--- my-app/src/components/EmployeeUnavailability.js
+++ my-app/src/components/EmployeeUnavailability.js
@@ -7,13 +7,17 @@ const EmployeeUnavailability = ({ emp }) => {
     const [showModal, setShowModal] = useState(false);
     const [editing, setEditing] = useState(null);   // contains row being edited
     const [showEditModal, setShowEditModal] = useState(false);
-
+    const [open, setOpen] = useState(false);
+    const [filter, setFilter] = useState("");
     const [form, setForm] = useState({
-        type: "Unavailable",
+        type: "",
         start_date: "",
         end_date: "",
+        all_day: false,
+        frequency_type: "none",
+        repeat_every: "",
         start_time: "",
-        end_time:"",
+        end_time: "",
         description: "",
     });
 
@@ -28,6 +32,7 @@ const EmployeeUnavailability = ({ emp }) => {
 
     // Form input handler
     const handleChange = (e) => {
+        console.log(e);
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
@@ -39,12 +44,14 @@ const EmployeeUnavailability = ({ emp }) => {
             body: JSON.stringify({
                 emp_id: emp.emp_id,
                 ...form,
+                start_time: form.all_day ? "00:00:00" : form.start_time,
+                end_time: form.all_day ? "23:59:00" : form.end_time
             }),
         });
 
         if (response.ok) {
             setShowModal(false);
-            setForm({ type: "Unavailable", start_date: "", end_date: "",start_time:"", end_time:"", description: "" });
+            setForm({ type: "", start_date: "", end_date: "", start_time: "", end_time: "", description: "" });
 
             // refresh table
             const res = await fetch(`http://127.0.0.1:5000/unavailability/${emp.emp_id}`);
@@ -117,19 +124,40 @@ const EmployeeUnavailability = ({ emp }) => {
 
         return endMin - startMin;
     }
+    const UNAVAILABILITY_TYPES = [
+        "Unavailability",
+        "Vacation FT Hourly - pay only",
+        "Float Day",
+        "Sick paid",
+        "Sick unpaid",
+        "Vacation PT and Casual - Seniority only",
+        "Lieu day for Stat worked (unpaid)",
+        "Unpaid leave + no seniority",
+        "ESA leave + seniority",
+        "Moved",
+        "WSIB Leave (with seniority)",
+        "Union",
+        "Domestic Violence Leave",
+        "Bereavement paid",
+        "Bereavement unpaid",
+        "Jury Duty",
+        "Vacation FT - seniority only",
+        "JHSC",
+        "Unavailable - First Aid CPR",
+        "No Show",
+        "Maternity/Paternity Leave"
+    ];
+
+    const filteredTypes = UNAVAILABILITY_TYPES.filter(t =>
+        t.toLowerCase().includes(filter.toLowerCase())
+    );
+
+
+
 
 
     return (
         <div className="d-flex mt-4">
-
-            {/* LEFT SIDEBAR */}
-            {/*<div className="border-end pe-4" style={{ width: "220px" }}>*/}
-            {/*    <div className="fw mb-2">Custom Pay Rates</div>*/}
-            {/*    <div className="fw text-primary mb-2">Unavailability</div>*/}
-            {/*    <div className="fw mb-2">Availability</div>*/}
-            {/*    <div className="fw mb-2">Employment Settings</div>*/}
-            {/*    <div className="fw mb-2">Notification Settings</div>*/}
-            {/*</div>*/}
 
             {/* RIGHT CONTENT */}
             <div className="flex-grow-1 ps-4">
@@ -210,6 +238,42 @@ const EmployeeUnavailability = ({ emp }) => {
 
                                 <h5 className="fw-bold">Add Unavailability</h5>
 
+                                <label>Type</label>
+                                <div className="dropdown">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Start typing..."
+                                        name="type"
+                                        value={open ? filter : form.type}
+                                        onFocus={() => setOpen(true)}
+                                        onChange={(e) => {
+                                            setFilter(e.target.value);
+                                            handleChange(e);
+                                            setOpen(true);
+                                        }}
+                                    />
+
+                                    {open && (
+                                        <ul className="dropdown-list">
+                                            {filteredTypes.map((t, idx) => (
+                                                <li key={idx} onClick={() => {
+                                                    handleChange({
+                                                        target: {
+                                                            name: "type",
+                                                            value: t
+                                                        }
+                                                    });
+                                                    setFilter("");
+                                                    setOpen(false);
+                                                }} className="list-unstyled">
+                                                    {t}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </div>
+
                                 <label className="mt-2">Start Date</label>
                                 <input
                                     type="text"
@@ -227,6 +291,23 @@ const EmployeeUnavailability = ({ emp }) => {
                                     value={form.end_date}
                                     onChange={handleChange}
                                 />
+
+                                <div className="checkbox-row">
+                                    <input
+                                        type="checkbox"
+                                        checked={form.allDay}
+                                        onChange={(e) =>
+                                            setForm(prev => ({
+                                                ...prev,
+                                                all_day: e.target.checked,
+                                                endTime: e.target.checked ? "" : prev.endTime
+                                            }))
+                                        }
+                                    />
+                                    <label>All day</label>
+                                </div>
+
+
                                 <label className="mt-2">Start Time</label>
                                 <input
                                     type="text"
@@ -240,22 +321,56 @@ const EmployeeUnavailability = ({ emp }) => {
                                 <input
                                     type="text"
                                     className="form-control"
+                                    disabled={form.all_day}
                                     name="end_time"
                                     value={form.end_time}
                                     onChange={handleChange}
                                 />
 
                                 <label className="mt-2">Description</label>
-                                <select
+                                <input
                                     type="text"
                                     className="form-control"
                                     name="description"
                                     value={form.description}
                                     onChange={handleChange}
+                                />
+
+                                <label>Frequency Type</label>
+                                <select
+                                    className="form-control"
+                                    name="frequency_type"
+                                    value={form.frequency_type}
+                                    onChange={handleChange}
                                 >
-                                    <option name="Sick">Sick</option>
-                                    <option name="Selected Shift Off">Selected Shift Off</option>
+                                    <option value="none">Does not repeat</option>
+                                    <option value="daily">Daily</option>
+                                    <option value="weekly">Weekly</option>
+                                    <option value="monthly">Monthly</option>
                                 </select>
+
+                                {form.frequency_type !== "none" && (
+                                    <>
+                                        <label>
+                                            Repeat every ({form.frequency_type === "daily"
+                                                ? "day(s)"
+                                                : form.frequency_type === "weekly"
+                                                    ? "week(s)"
+                                                    : "month(s)"})
+                                        </label>
+
+                                        <input
+                                            type="number"
+                                            min="1"
+                                            className="form-control"
+                                            name="repeat_every"
+                                            value={form.repeat_every}
+                                            onChange={handleChange}
+                                        />
+                                    </>
+                                )}
+
+
 
                                 <div className="d-flex justify-content-end mt-3 gap-2">
                                     <button className="btn btn-secondary" onClick={() => setShowModal(false)}>Cancel</button>
